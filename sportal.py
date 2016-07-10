7e76403a687af0d84fe2fa9d75d0708108cc66b4
Marco <marco.jendryczko@gmx.net>
2016-07-10 22:26:57 +0200

    fixed spelling

--- sportal.py
+++ sportal.py
@@ -1,7 +1,7 @@
 from os import system
 import sys
 season = sys.argv[1]
-day = season.argv[2]
+day = sys.argv[2]
 
 outFile = 'sportal-'+season+'-'+day+'.json'
 system('scrapy crawl sportal -o ' + outFile + ' -a season=' +season+ ' -a day='+ day)
